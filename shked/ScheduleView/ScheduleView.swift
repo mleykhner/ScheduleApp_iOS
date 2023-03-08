385fb93f494e011bd1f93ca9e3814d8752071cff
mleykhner <mleykhner@ya.ru>
2023-03-08 18:58:53 +0300

    ScheduleView updated

--- shked/ScheduleView/ScheduleView.swift
+++ shked/ScheduleView/ScheduleView.swift
@@ -17,10 +17,27 @@ struct ScheduleView: View {
     @State var showSettings: Bool = false
     @State var showInfoMessage: Bool = false
     
+    @State var headerHeight: CGFloat = CGFloat.zero
+    @State var scrollOffset: CGPoint = CGPoint.zero
+    
     var body: some View {
         ZStack(alignment: .bottom){
+            OffsetObservingScrollView(axes: .vertical, showsIndicators: false, offset: $scrollOffset){
+                    Spacer()
+                        .frame(height: headerHeight)
+                    showClasses(vm.getClassesForDate(selectedDay))
+                    Spacer()
+                        .frame(height: 223)
+                }
+                .refreshable{
+                    if !sm.loading {
+                        vm.reload()
+                    }
+            }
+            
+            
             VStack {
-                ScrollView(.vertical, showsIndicators: false){
+                VStack {
                     if sm.loading {
                         InfoMessageView()
                             .transition(.opacity)
@@ -31,6 +48,7 @@ struct ScheduleView: View {
                             .font(.custom("Unbounded", size: 32))
                             .fontWeight(.bold)
                             .frame(maxWidth: .infinity, alignment: .leading)
+                            
                         
                         Button {
                             showSettings.toggle()
@@ -49,24 +67,25 @@ struct ScheduleView: View {
                     }
                     .foregroundColor(Color(tm.getTheme().foregroundColor))
                     .padding(12)
-                    showClasses(vm.getClassesForDate(selectedDay))
-                        
                 }
-                //MARK: Починить
-                .refreshable{
-                    if !sm.loading {
-                        vm.reload()
+                .background(
+                    Color.clear
+                        .measureSize { size in
+                        headerHeight = size.height
                     }
-                }
+                )
+                .background(
+                    Rectangle()
+                        .foregroundColor(Color.clear)
+                        .background(Material.ultraThin.opacity(scrollOffset.y > 0 ? 1 : 0))
+                        .animation(.easeIn(duration: 0.3), value: scrollOffset.y)
+                )
+                Spacer()
             }
-            
-            
-            
             
             CalendarView(selectedDay: $selectedDay)
                 .padding(.bottom, 59)
         }
-        
     }
     
     func showClasses(_ classes: [ClassModel]) -> some View {
@@ -84,15 +103,99 @@ struct ScheduleView: View {
                 }
                 ClassView(classObject: classObject/*, colorId: 1*/)
             }
-//            .transition(.slide)
-//            .animation(.easeOut(duration: 0.1))
         }
-        
     }
 }
 
 struct ScheduleView_Previews: PreviewProvider {
     static var previews: some View {
         ScheduleView()
+    }
+}
+
+struct SizePreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    value = nextValue()
+  }
+}
+
+struct MeasureSizeModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content.background(GeometryReader { geometry in
+      Color.clear.preference(key: SizePreferenceKey.self,
+                             value: geometry.size)
+    })
+  }
+}
+
+extension View {
+  func measureSize(perform action: @escaping (CGSize) -> Void) -> some View {
+    self.modifier(MeasureSizeModifier())
+          .onPreferenceChange(SizePreferenceKey.self){ newValue in
+              action(newValue)
+              print("The new child size is: \(newValue)")
+          }
+  }
+}
+
+struct PositionObservingView<Content: View>: View {
+    var coordinateSpace: CoordinateSpace
+    @Binding var position: CGPoint
+    @ViewBuilder var content: () -> Content
+
+    var body: some View {
+        content()
+            .background(GeometryReader { geometry in
+                Color.clear.preference(
+                    key: PreferenceKey.self,
+                    value: geometry.frame(in: coordinateSpace).origin
+                )
+            })
+            .onPreferenceChange(PreferenceKey.self) { position in
+                self.position = position
+            }
+    }
+}
+
+private extension PositionObservingView {
+    enum PreferenceKey: SwiftUI.PreferenceKey {
+        static var defaultValue: CGPoint { .zero }
+
+        static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {
+            // No-op
+        }
+    }
+}
+
+struct OffsetObservingScrollView<Content: View>: View {
+    var axes: Axis.Set = [.vertical]
+    var showsIndicators = true
+    @Binding var offset: CGPoint
+    @ViewBuilder var content: () -> Content
+
+    private let coordinateSpaceName = UUID()
+
+    var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            PositionObservingView(
+                coordinateSpace: .named(coordinateSpaceName),
+                position: Binding(
+                    get: { offset },
+                    set: { newOffset in
+                        //withAnimation {
+                            offset = CGPoint(
+                                x: -newOffset.x,
+                                y: -newOffset.y
+                            )
+                        //}
+                        
+                    }
+                ),
+                content: content
+            )
+        }
+        .coordinateSpace(name: coordinateSpaceName)
     }
 }
